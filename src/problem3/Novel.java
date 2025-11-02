2bbcfc307263c1c4d79e2dc9fdfa70a7d6deac68
Aymen <67931880+Aymen-bk@users.noreply.github.com>
2025-11-03 00:19:02 +0100

    Simplify getPrice and setPrice methods

--- src/problem3/Novel.java
+++ src/problem3/Novel.java
@@ -7,14 +7,11 @@ public class Novel extends Book{
         this.price=price;
     }
 
-    public double getPrice() {
-        return price;
-    }
+    public double getPrice() {return price;}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    @Override
+    public void setPrice(double price) {this.price = price;}
+
+    
     public String toString() {
         return getNumEnrg()+" "+getTitle()+" "+getAuthor()+" "+getNbrPages()+"p "+getPrice()+"$";
     }
