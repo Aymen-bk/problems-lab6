c06b4075e6be5ed64d14fc12b1bc882d58d4487d
Aymen <67931880+Aymen-bk@users.noreply.github.com>
2025-11-02 23:53:35 +0100

    Refactor customer and transaction initialization

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -2,16 +2,14 @@ package problem2;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer1=new Customer("Souhaib");
-        Customer customer2=new Customer("Mohamed");
-        Bank bank=new Bank("bank1");
-        bank.addCustomer(customer1);
-        bank.addCustomer(customer2);
-        bank.addTransaction(10,customer1);
-        bank.addTransaction(20,customer2);
-        bank.addTransaction(-10,customer1);
-        bank.addTransaction(-10,customer2);
-
-
+        Customer c1 = new Customer("Ay");
+        Customer c2 =  new Customer("B");
+        Bank bank = new Bank("bank1");
+        bank.addCustomer(c1);
+        bank.addCustomer(c2);
+        bank.addTransaction(15,c1);
+        bank.addTransaction(17,c2);
+        bank.addTransaction(-5,c1);
+        bank.addTransaction(-10,c2);
     }
 }
